457c349f53f49217ae6814fbef96041ab5668cf2
Nick Bender <nickbender@gmail.com>
2026-07-09 22:06:45 +0000

    Update wistia cask to v2026.2.19

--- Casks/wistia.rb
+++ Casks/wistia.rb
@@ -1,6 +1,6 @@
 cask "wistia" do
-  version "2026.2.18"
-  sha256 "8b9281528191dc4caa936cbaaa20a3ec199c20e9875098fa37d78404096d2d9b"
+  version "2026.2.19"
+  sha256 "ebc7d61fe8f98ae4ac7ec117c7a4817d6f10cf7153011f00520999299f8ee4c5"
 
   url "https://wistia-glass-updates.s3.amazonaws.com/Glass-#{version}.dmg"
   name "Wistia"
